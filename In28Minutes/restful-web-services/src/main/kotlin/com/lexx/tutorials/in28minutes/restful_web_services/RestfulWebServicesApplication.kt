6bf4a935b3d7982b73cd51af88c49574532e80fe
Alex.Corghencea <alex.corghencea@kingfisher.com>
2025-09-02 11:28:12 +0300

    add In28Minutes rest services module

---
+++ In28Minutes/restful-web-services/src/main/kotlin/com/lexx/tutorials/in28minutes/restful_web_services/RestfulWebServicesApplication.kt
@@ -0,0 +1,11 @@
+package com.lexx.tutorials.in28minutes.restful_web_services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfulWebServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfulWebServicesApplication>(*args)
+}
---
+++ In28Minutes/restful-web-services/src/test/kotlin/com/lexx/tutorials/in28minutes/restful_web_services/RestfulWebServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lexx.tutorials.in28minutes.restful_web_services
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
